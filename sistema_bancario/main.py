ba927aa07cabae6ef88bd10ab2a38a367fb2f190
Ariel Juarez <85803503+arieljuarezdev@users.noreply.github.com>
2025-10-16 15:06:03 -0300

    Updates realized at work
    this need review

--- sistema_bancario/main.py
+++ sistema_bancario/main.py
@@ -66,16 +66,52 @@
 #         print("Operação inválida, por favor selecione novamente a operação desejada.")
 
 
-usuarios = []
-# usuarios = [{'nome': 'aaa', 'cpf': '111', 'dt_nasc': '123123123', 'logr': 'asdasd, dsdsdsdsd - asdasdasdasdasd - dddddd / dd'}]
-contas = []
+# usuarios = []
+# contas = []
+usuarios = [{'nome': 'aaa', 'cpf': '111', 'dt_nasc': '123123123', 'logr': 'asdasd, dsdsdsdsd - asdasdasdasdasd - dddddd / dd'}]
+contas = [{'n_ agendcia': '0001', 'n_conta': 1, 'usuario': 'aaa'}]
 
-def valida_usuario(cpf):
-    print(f"cpf: {cpf}")
+def criar_usuario(dados):
+    print("Usuário cadastrado!")
+    usuarios.append(dados)
+    criar_conta(dados['cpf']) 
+  
+def criar_conta(cpf):
+    index = len(contas)
+    if index <= 1:
+        index += 1
     
+    dados = {
+        "n_ agendcia": "0001",
+        "n_conta":index,
+        "usuario": cpf,
+        "saldo": 0,
+        "limite": 500,
+        "extrato": "",
+        "numero_saques": 0,
+        "LIMITE_SAQUES": 3,
+    }
+    contas.append(dados)
+ 
+def area_logada():
+    
+
+  
+def acesso():
+    print("========* Acesso a conta *========")
+    info = input("Informe o CPF:")  
+    
+    for usuario in usuarios:
+        if info == usuario['cpf']:
+            print("==========================")
+            area_logada()
+        else:
+            print("\n==========================")
+            print("Cpf informado, não encontrado, tente novamente!")
+            acesso()   
     
 def menu():
-    acesso = input("""
+    info = input("""
 =======================================
          Informe a operação:
         [a] Acessar conta.
@@ -83,27 +119,33 @@ def menu():
 =======================================
 Digite a opção e aperte enter: """)
 
-    match acesso:
+    match info:
         case "a":
             if usuarios != []:
-                valida_usuario(acesso) # TRABALHAR NISSO
+                acesso()
             else:
                 cad = input("""
 =======================================
 Não existem contas cadastradas.
 Deseja criar uma conta? [s/n]: """)
 
+        
                 if cad != "s" and cad != "n":
                     print("=======================================")
-                    print("Resposta inesperada, tente novamente mais tarde.")
+                    print("Resposta inesperada, tente novamente!.")
+                    menu()
                 else:
                     if cad == "n":
                         print("=======================================")
                         print("Muito obrigada(o) e até a próxima.")
                     else:
                         guarda_dados()
+                        
+        case "c":
+            guarda_dados()
+
 def guarda_dados():
-    print ("=======================================")
+    print ("============* Criar Conta *===============")
     print ("Informe seus dados:")
     nome = input("Nome completo:")
     cpf = input ("CPF (apenas números): ")
@@ -118,21 +160,26 @@ def guarda_dados():
     conf = input("Confirme, os dados estão corretos? [s/n]: ")
     if conf != "s" and conf != "n":
         print("=======================================")
-        print("Resposta inesperada, tente novamente mais tarde.")
+        print("Resposta inesperada, tente novamente.")
+        guarda_dados()
     else:
-        dados ={
-            "nome":nome,
-            "cpf":cpf,
-            "dt_nasc": dt_nasc,
-            "logr": f'{logr}, {num} - {bairro} - {cid} / {uf}'  
-        }
-        print("Usuário cadastrado!")
-        usuarios.append(dados)
-        print(usuarios)
-
-                    
+        if conf != "n":
+            dados ={
+                "nome":nome,
+                "cpf":cpf,
+                "dt_nasc": dt_nasc,
+                "logr": f'{logr}, {num} - {bairro} - {cid} / {uf}'  
+            }
+            criar_usuario(dados) 
+        
+        else:
+            print("Ok, insira novamente os dados.")
+            guarda_dados()  
 
 def main():
     menu()
 
+    print(usuarios)
+    print(contas)
+    
 main()
