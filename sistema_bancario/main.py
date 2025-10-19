7c23fda8e8cd9e0d97d1255b6a0011a14b36eda5
arieljuarezdev <arieljuarez.d@gmail.com>
2025-10-19 20:32:58 -0300

    add area_operacao ;P

--- sistema_bancario/main.py
+++ sistema_bancario/main.py
@@ -1,70 +1,3 @@
-# menu = """
-
-# [d] Depositar
-# [s] Sacar
-# [e] Extrato
-# [q] Sair
-
-# => """
-
-# saldo = 0
-# limite = 500
-# extrato = ""
-# numero_saques = 0
-# LIMITE_SAQUES = 3
-
-# while True:
-
-#     opcao = input(menu)
-
-#     if opcao == "d":
-#         valor = float(input("Informe o valor do depósito: "))
-
-#         if valor > 0:
-#             saldo += valor
-#             extrato += f"Depósito: R$ {valor:.2f}\n"
-
-#         else:
-#             print("Operação falhou! O valor informado é inválido.")
-
-#     elif opcao == "s":
-#         valor = float(input("Informe o valor do saque: "))
-
-#         excedeu_saldo = valor > saldo
-
-#         excedeu_limite = valor > limite
-
-#         excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-#         if excedeu_saldo:
-#             print("Operação falhou! Você não tem saldo suficiente.")
-
-#         elif excedeu_limite:
-#             print("Operação falhou! O valor do saque excede o limite.")
-
-#         elif excedeu_saques:
-#             print("Operação falhou! Número máximo de saques excedido.")
-
-#         elif valor > 0:
-#             saldo -= valor
-#             extrato += f"Saque: R$ {valor:.2f}\n"
-#             numero_saques += 1
-
-#         else:
-#             print("Operação falhou! O valor informado é inválido.")
-
-#     elif opcao == "e":
-#         print("\n================ EXTRATO ================")
-#         print("Não foram realizadas movimentações." if not extrato else extrato)
-#         print(f"\nSaldo: R$ {saldo:.2f}")
-#         print("==========================================")
-
-#     elif opcao == "q":
-#         break
-
-#     else:
-#         print("Operação inválida, por favor selecione novamente a operação desejada.")
-
 
 # usuarios = []
 # contas = []
@@ -98,13 +31,19 @@ def criar_conta(cpf):
  
 def area_logada(cpf):
     cnt_logada = []
+    cnt_op ={}
     for conta in contas:
         if conta['usuario'] == cpf:
             cnt_logada.append(conta) 
-           
-    for i, conta in enumerate(cnt_logada):
-        print(f"[{i}]: nº da conta:{conta["n_conta"]} | CPF: {conta["usuario"]}")
+            cnt_op = conta
 
+    for i, conta in enumerate(cnt_logada):
+        print(f"[{i}]: nº da conta:{conta['n_conta']} | CPF: {conta['usuario']}")
+
+    area_operacao(cnt_op)
+    
+
+def area_operacao(cnt_acessada):
     acs_conta = int(input("Selecione a conta que desenja acessar. (Ex.: 3) : "))
     cnt_acessada = contas[acs_conta]
     
@@ -115,13 +54,25 @@ def area_logada(cpf):
 [s] Sacar
 [e] Extrato
 [q] Sair
-
 Selecione a ação desejada:""")
     
     # print(f"conta logada: {cnt_logada}")
-    
+    match act:
+        case "d":
+            print("========* Deposito *==========")
+            valor = float(input("Informe o valor: R$ "))
+            dep = depositar(cnt_acessada, valor)
+            print(f"\n Novo saldo: {dep}")
+            print(f"\n conta: {cnt_acessada}")
 
-  
+def depositar(dados, valor_dep,/):
+    
+    
+    dados['saldo'] += valor_dep
+    print(f"dados em depositar: {dados}")
+
+
+ 
 def acesso():
     print("========* Acesso a conta *========")
     info = input("Informe o CPF:")  
